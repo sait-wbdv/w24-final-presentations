e0c7d4ba95a4203efbdaaf9b9190c56bc0d96c2f
Ashlyn Knox <ashlyn@lilyx.io>
2024-04-13 22:12:11 -0600

    initialized pages

---
+++ pages/agenda.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data: presentations } = await useFetch("/api/agenda");
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1>Agenda</h1>
+    </header>
+    <UContainer>
+      <!-- List all presenters here -->
+      <ul>
+        <li v-for="(presenter, index) in presentations" :key="index">
+          <PresentationCard :presenter />
+        </li>
+      </ul>
+    </UContainer>
+  </main>
+</template>
+
+<style scoped></style>
