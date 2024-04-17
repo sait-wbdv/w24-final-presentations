355672242d8677e4013ecf28492f3f240d901ef9
Ashlyn Knox <ashlyn@lilyx.io>
2024-04-16 19:33:43 -0600

    main pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,48 @@
-<script setup></script>
-
+<script setup>
+const { data: presentations } = await useFetch("/api/agenda");
+</script>
 <template>
-  <div>Page: index</div>
+  <main>
+    <header class="container mx-auto my-12">
+      <h1 class="text-8xl font-semibold">
+        <span class="block">Capstone</span> Presentation Exhibition
+      </h1>
+      <p class="text-2xl">Winter 2024</p>
+    </header>
+    <section class="container mx-auto">
+      <header class="mt-12 mb-8">
+        <h2 class="text-5xl">Agenda</h2>
+      </header>
+      <div>
+        <ul>
+          <li class="my-8">
+            <h3 class="text-3xl font-semibold mb-2">9-9:10 Check In</h3>
+            <p>Coffee will be provided.</p>
+          </li>
+          <li class="my-8">
+            <h3 class="text-3xl font-semibold mb-2">Opening Remarks</h3>
+            <p>From Ashlyn Knox and Temitope Elodimuor</p>
+          </li>
+          <li class="my-8">
+            <h3 class="text-3xl font-semibold mb-2">Presentations</h3>
+            <ol>
+              <li
+                v-for="(presentation, index) in presentations"
+                :key="index"
+                class="my-4"
+              >
+                <PresentationCard :presentation :index />
+              </li>
+            </ol>
+          </li>
+          <li class="my-8">
+            <h3 class="text-3xl font-semibold mb-2">After Party</h3>
+            <p>Pizza and soft drinks will be served</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
---
+++ pages/students/index.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main>
+    <header>
+      <h1 class="text-6xl">Fast Track Web Developer Students</h1>
+    </header>
+  </main>
+</template>
